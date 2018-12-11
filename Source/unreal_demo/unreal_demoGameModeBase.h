1384d810da3b0ca3fd09dbd06de517851ffb10a7
Patrick Dildine <wpdildine@gmail.com>
2018-12-10 19:26:59 -0500

    initial commit

---
+++ Source/unreal_demo/unreal_demoGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "unreal_demoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_DEMO_API Aunreal_demoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
